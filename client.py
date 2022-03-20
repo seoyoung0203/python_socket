c7ad7ea77c6f0120b737b2b9276ae7155474c737
seoyoung <50100040+seoyoung0203@users.noreply.github.com>
2022-03-21 00:17:07 +0900

    add threading

--- client.py
+++ client.py
@@ -1,14 +1,27 @@
 from socket import *
+import threading
+import time
+
+def send(sock):
+    while True:
+        msg = input('client >>>')
+        sock.send(msg.encode('utf-8'))
+
+def receive(sock):
+    while True:
+        data = sock.recv(1024)
+        print('받은 메세지: ', data.decode('utf-8'))
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect(('127.0.0.1', 3001))
 print('연결')
 
-while True:
-    msg = input('client >>>')
-    print('msg', msg)
-    clientSocket.send(msg.encode('utf-8'))
-    print('메세지 전송 완료')
+sender = threading.Thread(target=send, args=(clientSocket, ))
+receiver = threading.Thread(target=receive, args=(clientSocket, ))
 
-    data = clientSocket.recv(1024)
-    print('받은 메세지: ', data.decode('utf-8'))
+sender.start()
+receiver.start()
+
+while True:
+    time.sleep(1)
+    pass
--- server.py
+++ server.py
@@ -1,4 +1,16 @@
 from socket import *
+import threading
+import time
+
+def send(sock):
+    while True:
+        msg = input('server >>>')
+        sock.send(msg.encode('utf-8'))
+
+def receive(sock):
+    while True:
+        data = sock.recv(1024)
+        print('받은 메세지: ', data.decode('utf-8'))
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('127.0.0.1', 3001))
@@ -8,8 +20,12 @@ print("연결대기중...")
 connectionSocket, addr = serverSocket.accept()
 print('연결 주소: ', str(addr))
 
+sender = threading.Thread(target=send, args=(connectionSocket, ))
+receiver = threading.Thread(target=receive, args=(connectionSocket, ))
+
+sender.start()
+receiver.start()
+
 while True:
-    data = connectionSocket.recv(1024)
-    print('받은 메세지: ', data.decode('utf-8'))
-    msg = input('server >>>')
-    connectionSocket.send(msg.encode('utf-8'))
+    time.sleep(1)
+    pass
